bc56445f4781b0128f2ab760265f46f604ccafab
shuklansh <ansh.shukla270501@gmail.com>
2024-09-01 19:22:34 +0530

    Initial Commit

---
+++ iOS Camera App/iOS_Camera_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Camera_AppApp.swift
+//  iOS Camera App
+//
+//  Created by Ansh Shukla on 01/09/24.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Camera_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
